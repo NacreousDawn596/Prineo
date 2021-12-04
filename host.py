bf6460809066b019adc3f5626e16a9a7b5508d62
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2021-12-04 11:16:47 +0100

    Update host.py

--- host.py
+++ host.py
@@ -1,6 +1,9 @@
 import os
 for files in os.popen('ls ~/.local/share/NacreousDawn596/prineo/templates/').read().splitlines():
-	if 'htm' in files.split('.')[1] and files != "index.html":
-		os.system(f'mv ~/.local/share/NacreousDawn596/prineo/templates/{files} ~/.local/share/NacreousDawn596/prineo/templates/index.html')
+	try:
+		if 'htm' in files.split('.')[1] and files != "index.html":
+			os.system(f'mv ~/.local/share/NacreousDawn596/prineo/templates/{files} ~/.local/share/NacreousDawn596/prineo/templates/index.html')
+	except IndexError:
+		pass
 ip = os.popen("ip addr show | grep 'inet 19'").read().split('/')[0].split()[-1]
 os.system(f"cd ~/.local/share/NacreousDawn596/prineo/ && flask run -h {ip}")
